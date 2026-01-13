a25f686582cc6128b138c0421aacee38e3f9ec70
curranth <curranth@users.noreply.github.com>
2026-01-13 16:00:36 +0000

    Add files via upload

---
+++ celltype_enrichment/depict/run_depict_HT_SumGM_females.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+
+#SBATCH -A lindgren.prj 
+#SBATCH -J HT_SumGM_females_run-depict
+
+#SBATCH --output /well/lindgren/ferreira/PROJECTS/MRI/depict_analysis/logs/HT_SumGM_females_run_depict-%j.out 
+
+#SBATCH --error /well/lindgren/ferreira/PROJECTS/MRI/depict_analysis/logs/HT_SumGM_females_run_depict-%j.err 
+#SBATCH -p short 
+
+#SBATCH --cpus-per-task=1
+
+echo `date`: Executing task ${SGE_TASK_ID} of job ${JOB_ID} on `hostname` as user ${USER}
+
+##########################################################################################
+
+module purge 
+
+module load Python/2.7.18-GCCcore-10.2.0
+
+source /well/lindgren/ferreira/RESOURCES/venvs/DEPICT/bin/activate
+
+cd /well/lindgren/ferreira/PROJECTS/MRI
+
+
+        /well/lindgren/ferreira/RESOURCES/DEPICT/src/python/depict.py depict_analysis/config_files/HT_SumGM_females.cfg
+
+
+
+echo "###########################################################"
+echo "Finished at: "`date`
+echo "###########################################################"
+exit 0
